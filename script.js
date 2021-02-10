12cdda71ecaa48a0fc9de74b1501b27ebd7cea7e
almightysophie <77300753+almightysophie@users.noreply.github.com>
2021-02-10 03:37:26 -0500

    吗

--- script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $("button").click(function(){
-      $("#home").fadeToggle();
-      $("#minbox").fadeToggle("slow");
-      $("#maxbox").fadeToggle(3000);
-    });
-  });
